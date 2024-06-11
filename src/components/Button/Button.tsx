433d674d499a7b36c61b6c861f768485431cf494
naveengitboi <naveenjangiti9@gmail.com>
2024-06-11 14:38:11 +0530

    added initial publish 0.0.1 successfully tested...

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React, {ReactNode} from "react";
+
+interface Props{
+    children:ReactNode | ReactNode[]
+}
+
+const Button:React.FC<Props> = ({children}) => {
+    return(
+       <button>Naveen Au Btn{children}</button>
+    )
+}
+
+export default Button
