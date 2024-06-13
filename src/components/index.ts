d2318c7055dbb470e03b3b8a6b7991ce4bdfaa7d
naveengitboi <naveenjangiti9@gmail.com>
2024-06-13 13:16:25 +0530

    added infotext

--- src/components/index.ts
+++ src/components/index.ts
@@ -11,4 +11,5 @@
 export {default as Button} from './Button'
 export {default as ButtonIcon} from './ButtonIcon' 
 export {default as CodeBlock} from './CodeBlock'
+export {default as InfoText} from './Info' 
   //Navbars
